4c4d843eb1e046a514e29bc315859dffeb74609e
Nick <darkforceyoda@gmail.com>
2023-04-07 17:06:42 -0500

    Hero +Hero styles

---
+++ components/Main.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { BsPersonLinesFill } from 'react-icons/bs'
+import { AiOutlineMail } from 'react-icons/ai';
+
+const Main = () => {
+    return (
+        <div className='w-full h-screen text-center'>
+            <div className='max-w-[1240px] w-full h-full p-2 flex justify-center items-center'>
+                <div>
+                    <p className='uppercase text-sm tracking-widest pb-4 text-gray-600'>
+                        Looking to Improve your Company
+                    </p>
+                    <h1 className='text-gray-700'>
+                        I'm
+                        <span className='text-red-800 px-2'>
+                            Nicholas Abell
+                        </span>
+                    </h1>
+                    <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate cumque ipsam animi ratione mollitia laudantium qui veritatis, quaerat libero recusandae consequuntur quibusdam voluptatum aut, unde distinctio alias. Numquam, ab voluptates.
+                    </p>
+                    <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                            <FaGithub size={25} />
+                        </div>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                            <FaLinkedin size={25} />
+                        </div>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                            <AiOutlineMail size={25} />
+                        </div>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                            <BsPersonLinesFill size={25} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Main
