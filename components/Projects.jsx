7f32d850253b588ef273b86c7f697f4f62e387af
Nick <darkforceyoda@gmail.com>
2023-04-08 11:54:51 -0500

    +Projects comp

---
+++ components/Projects.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import testImg from '../assets/images/movie_database.png'
+import Project from './Project';
+
+
+const Projects = () => {
+    return (
+        //work on margin
+        <div className='w-full h-screen text-center flex flex-col justify-center items-center mt-24'>
+            <p className='text-xl tracking-widest uppercase text-red-800'>Projects</p>
+            <h2 className='py-4'>What I've Built</h2>
+            <div className='grid md:grid-cols-2 gap-8 px-4'>
+                <Project
+                    title='Movie Database'
+                    img={testImg} projectURL='/'
+                    info='Find Which streaming servives has your favorite movies.'
+                />
+                <Project
+                    title='Movie Database'
+                    img={testImg} projectURL='/'
+                    info='Find Which streaming servives has your favorite movies.'
+                />
+                <Project
+                    title='Movie Database'
+                    img={testImg} projectURL='/'
+                    info='Find Which streaming servives has your favorite movies.'
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Projects
