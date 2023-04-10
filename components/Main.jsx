e7731ec37e6bba46b9a79c4b2411d05b9b9a5648
Nick <darkforceyoda@gmail.com>
2023-04-09 22:39:08 -0500

    links added

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Link from 'next/link';
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import { BsPersonLinesFill } from 'react-icons/bs'
 import { AiOutlineMail } from 'react-icons/ai';
 
 const Main = () => {
+
     return (
         <div id='home' className='w-full h-screen text-center flex justify-center items-center'>
             <div className='max-w-[1240px] w-full h-full p-2 flex justify-center items-center'>
@@ -21,12 +23,12 @@ const Main = () => {
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate cumque ipsam animi ratione mollitia laudantium qui veritatis, quaerat libero recusandae consequuntur quibusdam voluptatum aut, unde distinctio alias. Numquam, ab voluptates.
                     </p>
                     <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
-                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                        <Link href='https://github.com/Nicholas-Abell' className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                             <FaGithub size={25} />
-                        </div>
-                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                        </Link>
+                        <Link href='https://www.linkedin.com/in/nicholas-abell-348807192/' className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                             <FaLinkedin size={25} />
-                        </div>
+                        </Link>
                         <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                             <AiOutlineMail size={25} />
                         </div>
