861a48b55ca38945eaa366235cb6159f3a09e65a
Nick <darkforceyoda@gmail.com>
2023-04-16 11:46:05 -0500

    darkmode attached to html class

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, createContext } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import Navbar from '@/components/Navbar';
 import './globals.css';
 
@@ -13,6 +13,15 @@ export const DarkModeContext = createContext();
 export default function RootLayout({ children }) {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+  useEffect(() => {
+    const html = document.querySelector('html');
+    if (isDarkMode) {
+      html.classList.add('dark');
+    } else {
+      html.classList.remove('dark');
+    }
+  }, [isDarkMode]);
+
   const toggleDarkMode = () => {
     setIsDarkMode(prevState => !prevState)
   }
@@ -20,11 +29,11 @@ export default function RootLayout({ children }) {
   return (
     <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
       <html lang="en">
-        <body className={isDarkMode ? 'bg-black' : 'bg-[#ECF0F3]'}>
+        <body className='dark:bg-black bg-[#ECF0F3]'>
           <Navbar />
           {children}
         </body>
-      </html>
+      </html>g
     </DarkModeContext.Provider>
   )
 }
