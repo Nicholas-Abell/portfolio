10f00a076c217e26e04146f0025526b33203f11b
Nick <darkforceyoda@gmail.com>
2023-04-16 00:50:16 -0500

    DarkModeContext Provider

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,30 @@
-import Navbar from '@/components/Navbar'
-import './globals.css'
+'use client';
+import React, { useState, createContext } from 'react';
+import Navbar from '@/components/Navbar';
+import './globals.css';
 
 export const metadata = {
   title: 'Nicholas Abell',
   description: 'Portfolio',
 }
 
+export const DarkModeContext = createContext();
+
 export default function RootLayout({ children }) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(prevState => !prevState)
+  }
+
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        {children}
-      </body>
-    </html>
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      <html lang="en">
+        <body className={isDarkMode ? 'bg-black' : 'bg-[#ECF0F3]'}>
+          <Navbar />
+          {children}
+        </body>
+      </html>
+    </DarkModeContext.Provider>
   )
 }
