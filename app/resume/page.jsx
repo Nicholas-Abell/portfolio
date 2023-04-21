fd01a0f540baf253241a7f5d824ed3675e6f075a
Nick <darkforceyoda@gmail.com>
2023-04-21 13:00:23 -0500

    sidebar, experience, education, skills

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,15 +1,16 @@
 'use client'
 import React from 'react';
+import { RxDot } from 'react-icons/rx';
 
 export default function Resume() {
     return (
-        <div className="bg-gray-100 text-gray-900 pt-20 flex">
-            <div className='w-[30vh] bg-blue-500 text-white py-4'>
+        <div className="bg-gray-100 text-gray-900 pt-20 flex print-ready">
+            <div className='w-[30vh] bg-blue-500 dark:bg-red-700 text-white py-4'>
                 <div className='p-2'>
                     <h2>Nick Abell</h2>
                     <p>Front End Developer</p>
                 </div>
-                <div className='bg-blue-800 p-2 my-2 w-full'>
+                <div className='bg-blue-800 dark:bg-red-900 p-2 w-full'>
                     <h3 className='font-extrabold uppercase'>Personal Info</h3>
                 </div>
                 <div className='p-2 flex flex-col'>
@@ -24,79 +25,119 @@ export default function Resume() {
                         </li>
                         <li>
                             <h3 className='py-2 underline'>GitHub</h3>
-                            <p className='text-gray-100'>asdfasdfsf</p>
+                            <a href='https://github.com/Nicholas-Abell' className='text-gray-100 text-sm'>https://github.com/Nicholas-Abell</a>
                         </li>
                         <li>
                             <h3 className='py-2 underline'>LinkedIn</h3>
-                            <p className='text-gray-100'>aldfjlaskdjf.com</p>
+                            <a className='text-gray-100 text-sm'>https://www.linkedin.com/in/nicholas-abell-348807192/</a>
                         </li>
                     </ul>
                 </div>
-                <div className='bg-blue-800 p-2 my-2 w-full'>
+                <div className='dark:bg-red-900g p-2 my-2 w-full'>
                     <h3 className='font-extrabold'>Skills</h3>
                 </div>
-                <div className='px-2 flex flex-col gap-2'>
-                    <h3>Phone #</h3>
-                    <h3>Phone #</h3>
-                    <h3>Phone #</h3>
-                    <h3>Phone #</h3>
+                <div className='py-2'>
+                    <h3 className='p-2 font-bold underline underline-offset-4'>Front End</h3>
+                    <div className='px-4 flex flex-col gap-2 text-gray-100'>
+                        <h3 className='flex items-center'><RxDot />HTML</h3>
+                        <h3 className='flex items-center'><RxDot />CSS / SCSS</h3>
+                        <h3 className='flex items-center'><RxDot />Javascript / Typescripy</h3>
+                        <h3 className='flex items-center'><RxDot />React</h3>
+                        <h3 className='flex items-center'><RxDot />Next Js</h3>
+                    </div>
+                </div>
+                <div className='py-2'>
+                    <h3 className='p-2 font-bold underline underline-offset-4'>Back End</h3>
+                    <div className='px-4 flex flex-col gap-2 text-gray-100'>
+                        <h3 className='flex items-center'><RxDot />Firebase</h3>
+                    </div>
+                </div>
+                <div className='py-2'>
+                    <h3 className='p-2 font-bold underline underline-offset-4'>Other</h3>
+                    <div className='px-4 flex flex-col gap-2 text-gray-100'>
+                        <h3 className='flex items-center'><RxDot />VBA</h3>
+                        <h3 className='flex items-center'><RxDot />Google Sheets</h3>
+                        <h3 className='flex items-center'><RxDot />Forklift</h3>
+                        <h3 className='flex items-center'><RxDot />Swing Reach</h3>
+                        <h3 className='flex items-center'><RxDot />Scissor Lift</h3>
+                    </div>
                 </div>
             </div>
             <div className="flex flex-col w-full">
-                <div className='px-8 py-2'>
+                <div className='px-8 pt-4'>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. In eos, culpa, similique, vel alias ipsum natus dolores quo sit repellendus dignissimos voluptate doloribus est fuga excepturi exercitationem quibusdam impedit ratione.
+                        A shipping/recieving clerk with an aptitude for technical learning. Having built several websites and web based applications I would like to turn my passion into a career.
                     </p>
                 </div>
-                <div className='w-full mt-4 border-t-2 border-b-2 border-gray-300'>
+                <div className='w-full mt-4 border-t-2 border-b-2 my-8 border-gray-300'>
                     <h1 className="text-3xl font-bold py-2 px-8">Experience</h1>
                 </div>
-                <div className='pl-8'>
-                    <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-8">
-                        <div>
-                            <h3 className="text-xl font-semibold mb-4">Contact</h3>
-                            <ul>
-                                <li className="mb-2"><span className="font-semibold">Phone:</span> Your Phone Number</li>
-                                <li className="mb-2"><span className="font-semibold">Email:</span> Your Email Address</li>
-                                <li className="mb-2"><span className="font-semibold">Website:</span> Your Website</li>
-                                <li className="mb-2"><span className="font-semibold">LinkedIn:</span> Your LinkedIn Profile</li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h3 className="text-xl font-semibold mb-4">Summary</h3>
-                            <p>Your summary goes here.</p>
+                <div className='pl-8 pb-8'>
+                    <div className="w-full">
+                        <div className='flex gap-32'>
+                            <h3 className="text-xl font-semibold mb-4">2011-<br />present</h3>
+                            <div className='flex flex-col gap-8'>
+                                <div>
+                                    <h2>Shipping/Recieving Clerk</h2>
+                                    <h3 className='text-red-700 text-2xl'>Valeo</h3>
+                                </div>
+                                <ul className='flex flex-col gap-4'>
+                                    <li>
+                                        <p>Record Data using Google Sheets, VBA, and Javascript</p>
+                                    </li>
+                                    <li>
+                                        <p>Use Barflow and SAP to recieve millions of dollars worth of product daily</p>
+                                    </li>
+                                    <li>
+                                        <p>Direct Semi drivers to proper location.</p>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                    <div className="mt-8">
-                        <h3 className="text-xl font-semibold mb-4">Skills</h3>
-                        <ul className="flex flex-wrap">
-                            <li className="bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded-full mr-2 mb-2">Skill 1</li>
-                            <li className="bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded-full mr-2 mb-2">Skill 2</li>
-                            <li className="bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded-full mr-2 mb-2">Skill 3</li>
-                            <li className="bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded-full mr-2 mb-2">Skill 4</li>
-                        </ul>
-                    </div>
-                    <div className="mt-8">
-                        <h3 className="text-xl font-semibold mb-4">Experience</h3>
-                        <div className="mb-4">
-                            <h4 className="text-lg font-semibold">Job Title</h4>
-                            <p className="text-md font-semibold">Company Name</p>
-                            <p className="text-md font-semibold">Date Range</p>
-                            <ul className="ml-4">
-                                <li className="mb-2">Experience 1</li>
-                                <li className="mb-2">Experience 2</li>
-                                <li className="mb-2">Experience 3</li>
-                            </ul>
+                </div>
+                <div className='pl-8 pb-8'>
+                    <div className="w-full">
+                        <div className='flex gap-32'>
+                            <h3 className="text-xl font-semibold mb-4">2022-<br />present</h3>
+                            <div className='flex flex-col gap-8'>
+                                <div>
+                                    <h2>Web Development</h2>
+                                    <h3 className='text-red-700 text-2xl'>Freelance</h3>
+                                </div>
+                                <ul className='flex flex-col gap-4'>
+                                    <li>
+                                        <p>Built a responsive website for a small local business</p>
+                                    </li>
+                                    <li>
+                                        <p>Hosted the site on Hostinger, and purchased domain name.</p>
+                                    </li>
+                                    <li>
+                                        <p>Made sure the site was accessible via the most popular search engines using SEO practices</p>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                        <div className="mb-4">
-                            <h4 className="text-lg font-semibold">Job Title</h4>
-                            <p className="text-md font-semibold">Company Name</p>
-                            <p className="text-md font-semibold">Date Range</p>
-                            <ul className="ml-4">
-                                <li className="mb-2">Experience 1</li>
-                                <li className="mb-2">Experience 2</li>
-                                <li className="mb-2">Experience 3</li>
-                            </ul>
+                    </div>
+                </div>
+                <div className='w-full mt-4 border-t-2 border-b-2 my-8 border-gray-300'>
+                    <h1 className="text-3xl font-bold py-2 px-8">Education</h1>
+                </div>
+                <div className='pl-8 pb-8'>
+                    <div className="w-full">
+                        <div className='flex gap-32'>
+                            <h3 className="text-xl font-semibold mb-4">2011-<br />present</h3>
+                            <div className='flex flex-col gap-8'>
+                                <div>
+                                    <h2>High School Diploma</h2>
+                                    <h3 className='text-red-700 text-2xl'>Jennings County High School</h3>
+                                </div>
+                                <ul className='flex flex-col gap-4'>
+                                    <li>
+                                        <p>School News Paper Editor</p>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
