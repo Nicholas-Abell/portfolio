bf0a494a1d1cde4ab7681f8ef7869b85560b0f39
Nick <darkforceyoda@gmail.com>
2023-04-22 20:58:46 -0500

    Navbar closes when email icon is clicked and redirects to home page

--- components/IconPalette.jsx
+++ components/IconPalette.jsx
@@ -1,25 +1,30 @@
+'use client'
 import React from 'react';
 import Icon from './Icon';
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import { BsPersonLinesFill } from 'react-icons/bs'
 import { AiOutlineMail } from 'react-icons/ai';
-import { HiChevronDoubleUp } from 'react-icons/hi';
 
 const IconPalette = ({ setMobileNav, nav }) => {
 
     const scrollToEmailForm = (id) => {
-        nav ? setMobileNav(false) : null;
+        nav ? setMobileNav(false) : null
         const skillsElement = document.getElementById(id);
-        const offset = 90;
-        const yCoordinate = skillsElement.getBoundingClientRect().top + window.pageYOffset - offset;
-        window.scrollTo({ top: yCoordinate, behavior: 'smooth' })
+        if (skillsElement) {
+            const offset = 90;
+            const yCoordinate = skillsElement.getBoundingClientRect().top + window.pageYOffset - offset;
+            window.scrollTo({ top: yCoordinate, behavior: 'smooth' });
+        } else {git 
+            window.location.href = '/';
+        }
     }
+
 
     return (
         <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
             <Icon reactIcon={<FaGithub size={25} />} url={'https://github.com/Nicholas-Abell'} />
             <Icon reactIcon={<FaLinkedin size={25} />} url={'https://www.linkedin.com/in/nicholas-abell-348807192/'} />
-            <div onClick={() => scrollToEmailForm('contact')} className='neon rounded-full cursor-pointer p-3'>
+            <div onClick={() => scrollToEmailForm('email')} className='neon rounded-full cursor-pointer p-3'>
                 <AiOutlineMail size={25} />
             </div>
             <Icon reactIcon={<BsPersonLinesFill size={25} />} url={'/resume'} />
