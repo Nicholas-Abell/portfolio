c1715c2ad1f2bf463b36b30b425e2b035dd55807
Nick <darkforceyoda@gmail.com>
2023-04-23 23:06:05 -0500

    test 3

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
-// next.config.js
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    images: true,
+    appDir: true,
   },
-};
+}
 
-module.exports = nextConfig;
-
-module.exports = {
-  // Other Next.js configurations...
-  async exportPathMap(defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-    const paths = await defaultPathMap();
-    delete paths['/api/*']; // Exclude API routes
-    return paths;
-  },
-};
+module.exports = nextConfig
