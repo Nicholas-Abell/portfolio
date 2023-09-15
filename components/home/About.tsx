381f2a68ee3e217bd98850c8a2d7fdb5018a7a76
Nick Abell <darkforceyoda@gmail.com>
2023-09-15 08:53:31 -0700

    About section layout design, Bio, Languages, Currency

--- components/home/About.tsx
+++ components/home/About.tsx
@@ -67,7 +67,7 @@ const About = () => {
         </div>
       </div>
       <div className="grid grid-cols-3 justify-center gap-2 p-4">
-        <div className="border border-gray-400 grid grid-cols-2 justify-center items-center gap-4">
+        <div className="border border-gray-400 grid grid-cols-2 justify-center gap-4 p-4">
           <BsFillHeartFill size={50} className="m-auto" />
           <BiSolidShield size={60} className="m-auto" />
           <div className="bg-gray-600 py-12">c</div>
@@ -75,11 +75,66 @@ const About = () => {
           <div className="bg-gray-600 py-12">c</div>
           <div className="bg-gray-600 py-12">c</div>
           <div className="bg-gray-600 col-span-2">c</div>
-          <div className="bg-gray-600 col-span-2 py-12">c</div>
+          <div className="bg-gray-400 border-4 flex flex-col justify-between col-span-2">
+            <div className="flex justify-center items-center w-full h-full">
+              <p className="p-8">Nagotiable</p>
+            </div>
+            <p className="text-center font-bold bottom-0 uppercase py-4 border-t-2">
+              CURRENCY
+            </p>
+          </div>
         </div>
 
-        <div className="border border-gray-400">c</div>
-        <div className="border border-gray-400">c</div>
+        <div className="border border-gray-400 grid grid-cols-1 justify-center p-4">
+          <div className="grid grid-cols-3 border-b-2">
+            <div className="bg-red-800 p-4"></div>
+            <div className="bg-gray-300 col-span-2 p-4"></div>
+          </div>
+          <div className="grid grid-cols-3 border-b-2">
+            <div className="bg-red-800 p-4"></div>
+            <div className="bg-gray-300 col-span-2 p-4"></div>
+          </div>
+          <div className="grid grid-cols-3 border-b-2">
+            <div className="bg-red-800 p-4"></div>
+            <div className="bg-gray-300 col-span-2 p-4"></div>
+          </div>
+          <div className="grid grid-cols-3 border-b-2">
+            <div className="bg-red-800 p-4"></div>
+            <div className="bg-gray-300 col-span-2 p-4"></div>
+          </div>
+          <div className="grid grid-cols-3 border-b-2">
+            <div className="bg-red-800 p-4"></div>
+            <div className="bg-gray-300 col-span-2 p-4"></div>
+          </div>
+          <div className="grid grid-cols-3">
+            <div className="bg-red-800 p-4"></div>
+            <div className="bg-gray-300 col-span-2 p-4"></div>
+          </div>
+        </div>
+
+        <div className="border border-gray-400 grid p-2 rounded-lg">
+          <div className="bg-gray-400 border-4 flex flex-col justify-between">
+            <div className="flex justify-center items-center w-full h-full">
+              <p className="pl-4">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic
+                culpa tempore an asdfasd fsdf asdf sadf hfghpfpgiojear
+                sdcxzp'cogjvar twseiofjs
+              </p>
+            </div>
+            <p className="text-center font-bold bottom-0 uppercase py-4 border-t-2">
+              BIO
+            </p>
+          </div>
+          <div className="bg-gray-400 border-4 grid grid-cols-1 ">
+            <p className="border-b-2 pt-4 pl-4">English - fluent</p>
+            <p className="border-b-2 pt-4 pl-4">Spanish - basic</p>
+            <span className="border-b-2 p-4"></span>
+            <span className="border-b-2 p-4"></span>
+            <p className="text-center font-bold bottom-0 uppercase pt-4">
+              Proficiencies & Languages
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
