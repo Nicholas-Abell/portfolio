8faf998d3716a0ed72a4b2137eace05ffc918c79
Nick Abell <darkforceyoda@gmail.com>
2023-09-17 12:38:43 -0700

    added bio, and equipment section

--- components/about/Biography.tsx
+++ components/about/Biography.tsx
@@ -9,26 +9,64 @@ const Biography: React.FC<BiographyProps> = ({ sheetNumber }) => {
     <div
       className={`${
         sheetNumber === 3 ? "grid" : "hidden"
-      } lg:grid border border-gray-400 p-2 bg-gray-200 rounded-lg`}
+      } lg:grid border border-gray-400 p-2 bg-gray-200 rounded-lg gap-4`}
     >
-      <div className="bg-gray-400 border-4 flex flex-col justify-between">
-        <div className="flex justify-center items-center w-full h-full">
-          <p className="px-4">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic culpa
-            tempore an asdfasd fsdf asdf sadf hfghpfpgiojear sdcxzp'cogjvar
-            twseiofjs
-          </p>
-        </div>
-        <p className="text-center font-bold uppercase py-4 border-t-2">BIO</p>
-      </div>
-      <div className="bg-gray-400 border-4 grid grid-cols-1 ">
-        <p className="border-b-2 pt-4 pl-4">English - fluent</p>
-        <p className="border-b-2 pt-4 pl-4">Spanish - basic</p>
-        <span className="border-b-2 p-4"></span>
-        <span className="border-b-2 p-4"></span>
-        <p className="text-center font-bold bottom-0 uppercase pt-4">
-          Proficiencies & Languages
+      <div className="bg-white grid grid-cols-1 border-2 border-gray-400 col-span-2 rounded-lg">
+        <p className="w-full border-black border-b text-center">
+          A software developer looking to
         </p>
+        <p className="w-full border-black border-b px-2 text-center">
+          expand his resume. I make mobile
+        </p>
+        <p className="w-full border-black border-b px-2 text-center">
+          responsive websites primarily using
+        </p>
+        <p className="w-full border-black border-b px-2 text-center">
+          NextJs and tailwind css, but
+        </p>
+        <p className="w-full border-black border-b px-2 text-center">
+          am always excited to learn new
+        </p>
+        <p className="w-full border-black border-b px-2 text-center">
+          frameworks or tech stacks.
+        </p>
+        <p className="text-center font-bold uppercase border-t-2">bio</p>
+      </div>
+      <div className="bg-white grid grid-cols-1 grid-rows-6 border-2 border-gray-400 col-span-2 rounded-lg">
+        <div className="w-full border-black border-b"></div>
+        <div className="w-full border-black border-b"></div>
+        <p className="w-full border-black border-b text-center">
+          English - Fluent
+        </p>
+        <p className="w-full border-black border-b text-center">
+          Spanish - Basic
+        </p>
+        <div className="w-full border-black border-b"></div>
+        <p className="text-center font-bold uppercase border-t-2">Languages</p>
+      </div>
+      <div className="bg-white grid grid-cols-1 grid-rows-6 border-2 border-gray-400 col-span-2 rounded-lg gap-2">
+        <div className="grid grid-cols-4 px-4 text-gray-400 gap-4">
+          <p className=" col-span-2 text-xs">NAME</p>
+          <p className=" col-span-1 text-xs">ATK BONUS</p>
+          <p className=" col-span-1 text-xs">DMG/TYPE</p>
+        </div>
+        <div className="grid grid-cols-4 px-4 text-gray-400 gap-4">
+          <div className="col-span-2 bg-gray-200 rounded-tl-lg rounded-br-lg"></div>
+          <div className="col-span-1 bg-gray-200 rounded-tl-lg rounded-br-lg"></div>
+          <div className="col-span-1 bg-gray-200 rounded-tl-lg rounded-br-lg"></div>
+        </div>
+        <div className="grid grid-cols-4 px-4 text-gray-400 gap-4">
+          <div className="col-span-2 bg-gray-200 rounded-tl-lg rounded-br-lg"></div>
+          <div className="col-span-1 bg-gray-200 rounded-tl-lg rounded-br-lg"></div>
+          <div className="col-span-1 bg-gray-200 rounded-tl-lg rounded-br-lg"></div>
+        </div>
+        <div className="grid grid-cols-4 px-4 text-gray-400 gap-4">
+          <div className="col-span-2 bg-gray-200 rounded-tl-lg rounded-br-lg"></div>
+          <div className="col-span-1 bg-gray-200 rounded-tl-lg rounded-br-lg"></div>
+          <div className="col-span-1 bg-gray-200 rounded-tl-lg rounded-br-lg"></div>
+        </div>
+        <div className="w-full border-black border-b"></div>
+        <p className="text-center font-bold uppercase">EQUIPMENT</p>
       </div>
     </div>
   );
