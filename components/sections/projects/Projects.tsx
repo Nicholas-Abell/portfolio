eb3fb174b8b402264707715ce1542f3ef097492e
Nick <darkforceyoda@gmail.com>
2023-10-03 18:33:09 -0500

    trimmed grid col

--- components/sections/projects/Projects.tsx
+++ components/sections/projects/Projects.tsx
@@ -15,7 +15,7 @@ const Projects = () => {
   return (
     <div className="w-full h-auto text-center" id="projects">
       <Title title="PROJECTS" />
-      <div className="grid grid-cols-1 grid-rows-1 lg:grid-cols-2 xl:grid-cols-3 xl:px-12 gap-4 justify-center items-center mt-24 p-4 overflow-hidden">
+      <div className="grid grid-cols-1 grid-rows-1 lg:grid-cols-2 gap-8 justify-center items-center mt-24 p-4 overflow-hidden">
         <MovieDatabase />
         <ThistleandThorns />
         <Reddit />
@@ -28,7 +28,7 @@ const Projects = () => {
       </div>
       <button
         onClick={() => setMore(true)}
-        className={`bg-gray-600 w-[80%] mx-auto py-2 rounded-lg text-white font-bold hover:bg-slate-500/60 ease-in duration-200 ${
+        className={`bg-gray-600 w-[80%] mx-auto mt-8 py-2 rounded-lg text-white font-bold hover:bg-slate-500/60 ease-in duration-200 ${
           more ? "hidden" : "block"
         }`}
       >
